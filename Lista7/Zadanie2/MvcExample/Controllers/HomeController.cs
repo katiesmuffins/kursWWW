4d042a4ba0fe7d9e3b9a5639f8a3b0612f40085a
katiesmuffins <33500192+katiesmuffins@users.noreply.github.com>
2017-12-16 20:45:03 +0100

    Add files via upload

---
+++ Lista7/Zadanie2/MvcExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcExample.Models;
+using MvcExample.Services;
+
+namespace MvcExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPersonRepository _personRepository;
+
+        public HomeController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            var model = _personRepository.GetPage(page);
+            ViewData["Title"] = "Lista osób";
+            ViewBag.Title = "Lista osób";
+            //return Content("Hello");
+            //return new ObjectResult(model);
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = _personRepository.GetById(id);
+            ViewData["Title"] = model.FirstName + " " + model.LastName;
+            return View(model);
+
+        }
+    }
+}
---
+++ Lista7/Zadanie2/MvcExample/Services/PersonRepositoryIM.cs
@@ -0,0 +1,44 @@
+﻿using MvcExample.Models;
+using System.Collections.Generic;
+
+namespace MvcExample.Services
+{
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> GetPage(int page);
+        Person GetById(int id);
+    }
+    public class PersonRepositoryIM : IPersonRepository
+    {
+        List<Person> _persons;
+        int pageSize = 2;
+
+        public PersonRepositoryIM()
+        {
+            _persons = new List<Person>
+            {
+                new Person { Id = 1, FirstName = "Jan", LastName = "Kowalski", Address = "ul. Trawiasta 10" },
+                new Person { Id = 2, FirstName = "Agata", LastName = "Zielony", Address = "ul. Koszykowa 12" },
+                new Person { Id = 3, FirstName = "Ewa", LastName = "Szumska", Address = "ul. Zaciszna 3" },
+                new Person { Id = 4, FirstName = "Kasia", LastName = "Iwankiewicz", Address = "ul. Bajkowa 1"},
+                new Person { Id = 5, FirstName = "Wojtek", LastName = "Mik", Address = "ul. Nowa 3"},
+                new Person { Id = 6, FirstName = "Ania", LastName = "Goś", Address = "ul. Nożownicza 1"}
+
+            };
+        }
+
+        public Person GetById(int id)
+        {
+            foreach (var p in _persons)
+                if (p.Id == id)
+                    return p;
+            return null;
+        }
+        public IEnumerable<Person> GetPage(int page)
+        {
+            var first = (page - 1) * pageSize;
+
+            return _persons.GetRange(first, pageSize);
+        }
+    }
+}
