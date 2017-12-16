38f83aee4ebbca6d0765dbca93f8c65299d9ab88
katiesmuffins <33500192+katiesmuffins@users.noreply.github.com>
2017-12-16 20:50:36 +0100

    Add files via upload

---
+++ Lista7/Zadanie 4/FormValidation/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormValidation.Models;
+
+namespace FormValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+            else
+            {
+            }
+            return View();
+        }
+        public ActionResult Success()
+        {
+            return View("Views/Home/Success.cshtml"); 
+        }
+    }
+}
---
+++ Lista7/Zadanie 4/FormValidation/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormValidation.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Podaj numer rejestracyjny")]
+        [RegularExpression("[A-Z]{2,3}[0-9]{4,6}", ErrorMessage = "Rejestracja jest niepoprawna")]
+        public string Rejestracja { get; set; }
+
+        [Required(ErrorMessage = "Podaj datę pierwszej rejestracji")]
+        [DataType(DataType.Date)]
+        public string Data { get; set; }
+
+        [Required(ErrorMessage = "Podaj markę pojazdu")]
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "Podana marka jest niepoprawna")]
+        public string Marka { get; set; }
+
+        [Required(ErrorMessage = "Podaj rok proodukcji")]
+        [RegularExpression("[0-9]{4}", ErrorMessage = "Podany rok produkcji jest niepoprawny")]
+        public string RokProdukcji { get; set; }
+
+        [Required(ErrorMessage = "Podaj rodzaj paliwa")]
+        [RegularExpression("P|ON|LPG|EE", ErrorMessage = "Podany rodzaj paliwa jest niepoprawny")]
+        public string RodzajPaliwa { get; set; }
+    }
+}
